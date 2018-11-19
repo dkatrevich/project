76cbcbc07b7497766e12105609a04a60855a46f1
Alexander <mozhega@rozetka.com.ua>
2018-11-19 18:09:04 +0200

    need fix

--- src/js/start.js
+++ src/js/start.js
@@ -1,232 +1,159 @@
-const { PI, cos, acos, sin, asin, abs, sqrt, pow, floor, round, random, atan2 } = Math;
-const HALF_PI = 0.5 * PI;
-const TAU = 2 * PI;
-const QT3_TAU = TAU - HALF_PI;
-const TO_RAD = PI / 180;
-const rand = n => n * random();
-const randRange = n => n - rand(2 * n);
-const fadeIn = (t, m) => t / m;
-const fadeOut = (t, m) => (m - t) / m;
-const fadeInOut = (t, m) => {
-    let hm = 0.5 * m;
-    return abs((t + hm) % m - hm) / hm;
-};
-const dist = (x1, y1, x2, y2) => sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-const angle = (x1, y1, x2, y2) => atan2(y2 - y1, x2 - x1);
-const lerp = (n1, n2, speed) => (1 - speed) * n1 + speed * n2;
 
-const deflectorCount = 50;
-const particleCount = 500;
 
-let canvas;
-let ctx;
-let origin;
-let mouse;
-let hover;
-let deflectors;
-let particles;
+(function($) {
 
-function setup() {
-    canvas = document.createElement('canvas');
-    ctx = canvas.getContext('2d');
-    canvas.style = `
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    `;
-    document.body.appendChild(canvas);
-    origin = {
-        x: 0,
-        y: 0
-    };
-    mouse = {
-        x: 0,
-        y: 0
-    };
-    hover = false;
-    init();
-    draw();
-}
+    var jstar_call_id = 0;
 
-function init() {
-    resize();
-    hover = false;
+    $.jstars = {};
+    $.fn.jstars = function(settings)
+    {
+        // check IE. only IE9+ support
+        var ua = window.navigator.userAgent,
+            msie = ua.indexOf("MSIE ");
+        if (msie > 0) {
+            if (parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))) < 9) return;
+        }
+        // apply default params
+        settings = $.extend({}, $.fn.jstars.defaults, settings);
+        // define frequency
+        settings.frequency = 20 - Math.max(1, Math.min(settings.frequency, 19));
 
-    deflectors = [];
-    for(let i = 0; i < deflectorCount; i++) {
-        deflectors.push(getDeflector());
-    }
+        // save in global
+        var jstar_timer = null;
+        var jstar_index = 0;
+        var jstar_dindex = 0;
+        var jstar_image = null;
+        var jstar_id = 'jstar_span_' + jstar_call_id++;
 
-    particles = [];
-    for(let i = 0; i < particleCount; i++) {
-        particles.push(getParticle(i).create());
-    }
-}
+        return this.each(
+            function()
+            {
+                // preprocess
+                if( !jstar_timer ){
+                    // timer function
+                    var jstar_uptade_star_bg = function(){
+                        if( ! $('span.jstar_span').size ) return;
 
-function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    origin.x = mouse.x = 0.5 * canvas.width;
-    origin.y = mouse.y = 0.5 * canvas.height;
-}
+                        $('span.jstar_span.'+jstar_id).each(function(){
+                            var bg_pos = $(this).css('background-position').split(' ');
+                            var bg_pos_x = parseInt(bg_pos[0]);
+                            var bg_pos_y = parseInt(bg_pos[1]);
+                            $(this).css('background-position', (bg_pos_x - settings.width) + 'px ' + bg_pos_y + 'px');
+                        })
+                    }
 
-function mouseHandler(e) {
-    hover = e.type === "mousemove";
-    mouse.x = e.clientX;
-    mouse.y = e.clientY;
-}
+                    // init timer
+                    jstar_timer = setInterval(jstar_uptade_star_bg, settings.delay / 9);
+                    // init image
+                    jstar_image = new Image();
+                    jstar_image.src = settings.image_path + '/' + settings.image;
+                }
 
-function getDeflector() {
-    return {
-        position: {
-            x: rand(window.innerWidth),
-            y: rand(window.innerHeight)
-        },
-        velocity: {
-            x: randRange(1),
-            y: randRange(1)
-        },
-        threshold: rand(200) + 100,
-        direction: rand(TAU),
-        move() {
-            if (this.position.x > canvas.width || this.position.x < 0) {
-                this.velocity.x *= -1;
+                // hover event
+                function hoverEvent(e) {
+                    if( (jstar_dindex++ % settings.frequency) != 0) return;
+
+                    // define what side we need to show stars:
+                    var sideX = jstar_rand(-1,1);
+                    var sideY = jstar_rand(-1,1);
+
+                    var randX = jstar_rand(5, 30);
+                    var randY = jstar_rand(5, 30);
+
+                    var opacity = Math.min(Math.random() + 0.4, 1);
+
+                    // calculate coordinate
+                    var touch;
+
+                    if (e.touches) {
+                        touch = e.touches[0];
+                    }
+
+                    var pageX = e.pageX || touch.pageX;
+                    var pageY = e.pageY || touch.pageY;
+
+                    var x = pageX + (sideX * randX);
+                    var y = pageY + (sideY * randY);
+
+                    // show span and launch animate
+                    var id = 'jstar_' + jstar_index++;
+
+                    // append style
+                    if( settings.style != 'rand' ){
+                        var bg_pos = '0px ' + settings.style_map[ settings.style ] + 'px';
+                    }
+                    else{
+                        var ind = jstar_rand(0, 5);
+                        var i = 0;
+                        for(var key in settings.style_map){
+                            if( i++ == ind ){
+                                var bg_pos = '0px ' + settings.style_map[ key ] + 'px';
+                                break;
+                            }
+                        }
+                    }
+
+                    var span = '<span id="' + id + '" class="jstar_span '+jstar_id+'" style="display:block; width:27px; height:27px; background:url('+jstar_image.src+') no-repeat '+bg_pos+'; margin:0; padding:0; position:absolute; top:-50px; left:0; pointer-events: none; touch-action: auto;">&nbsp;</span>';
+                    $(document.body).append(span);
+
+                    var star = $('#' + id);
+                    star
+                        .css({
+                            top: y,
+                            left: x,
+                            'opacity': opacity
+                        })
+                        .animate({ opacity: 0 }, settings.delay, function(){ star.remove(); }); // remove span on finish animate*/
+                }
+
+                if ('ontouchmove' in window) {
+                    this.addEventListener('touchmove', function (e) {
+                        hoverEvent(e);
+                    });
+
+                    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                        this.addEventListener('mousemove', function (e) {
+                            hoverEvent(e);
+                        });
+                    }
+                } else {
+                    this.addEventListener('mousemove', function (e) {
+                        hoverEvent(e);
+                    });
+                }
             }
-            if (this.position.y > canvas.height || this.position.y < 0) {
-                this.velocity.y *= -1;
-            }
-            this.position.x += this.velocity.x;
-            this.position.y += this.velocity.y;
-        }
+        )
     };
-}
 
-function getParticle(i) {
-    return {
-        create() {
-            this.position.x = this.lastPosition.x = origin.x + randRange(1);
-            this.position.y = this.lastPosition.x = origin.y + randRange(1);
-            this.speed = rand(5) + 1;
-            this.size = rand(3) + 0.5;
-            this.life = 0;
-            this.ttl = rand(100);
-            this.hue = randRange(30);
-            this.direction = angle(0.5 * canvas.width, 0.5 * canvas.height, this.position.x, this.position.y);
-            return this;
-        },
-        position: {
-            x: 0,
-            y: 0
-        },
-        lastPosition: {
-            x: 0,
-            y: 0
-        },
-        velocity: {
-            x: 0,
-            y: 0
-        },
-        update() {
-            this.life++;
-            this.lastPosition.x = this.position.x;
-            this.lastPosition.y = this.position.y;
-            this.velocity.x = lerp(
-                this.velocity.x,
-                cos(this.direction) * fadeInOut(this.life, this.ttl) * this.speed,
-                0.15
-            );
-            this.velocity.y = lerp(
-                this.velocity.y,
-                sin(this.direction) * fadeInOut(this.life, this.ttl) * this.speed,
-                0.15
-            );
-            this.position.x += this.velocity.x;
-            this.position.y += this.velocity.y;
-            this.life > this.ttl && this.create();
-        },
-        draw() {
-            this.update();
-            ctx.beginPath();
-            ctx.lineWidth = this.size;
-            ctx.strokeStyle = `hsla(${this.hue},60%,50%,${fadeInOut(this.life, this.ttl) * 0.5})`;
-            ctx.moveTo(this.lastPosition.x, this.lastPosition.y);
-            ctx.lineTo(this.position.x, this.position.y);
-            ctx.stroke();
-            ctx.closePath();
-        }
-    }
-}
+    /**
+     * helper functions
+     */
 
-let deflector, particle;
-
-function draw() {
-    let i, j;
-    origin.x =
-        lerp(
-            origin.x,
-            hover ? mouse.x : 0.5 * canvas.width,
-            0.05
-        );
-    origin.y =
-        lerp(
-            origin.y,
-            hover ? mouse.y : 0.5 * canvas.height,
-            0.05
-        );
-    ctx.fillStyle = "rgba(0,0,0,0.05)"
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-    for(i = particles.length - 1; i >= 0; i--) {
-        particle = particles[i];
-        for(j = deflectors.length - 1; j >= 0; j--) {
-            deflector = deflectors[j];
-
-            i === 0 && deflector.move();
-
-            particle.direction =
-                dist(
-                    particle.position.x,
-                    particle.position.y,
-                    deflector.position.x,
-                    deflector.position.y
-                ) < deflector.threshold
-                && lerp(
-                particle.direction,
-                angle(
-                    deflector.position.x,
-                    deflector.position.y,
-                    particle.position.x,
-                    particle.position.y
-                ) + angle(
-                origin.x,
-                origin.y,
-                particle.position.x,
-                particle.position.y
-                ),
-                0.075
-                )
-                || particle.direction;
-        }
-
-        particle.draw();
+// math rand in interval
+    function jstar_rand(from, to){
+        var r = Math.random();
+        r = r * (to - from);
+        r = r + from;
+        r = Math.round(r);
+        return r;
     }
 
-    ctx.save();
-    ctx.globalCompositeOperation = "lighten";
-    ctx.filter = "blur(6px)";
-    ctx.drawImage(canvas, 0, 0);
-    ctx.restore();
+    $.fn.jstars.defaults = {
+        image_path: '', // this is requried param
+        image: 'jstar-modern.png', // this is requried param
+        style: 'white',
+        frequency: 12,
+        style_map: {
+            white: 0,
+            blue: -25,
+            green: -50,
+            red: -75,
+            yellow: -100
+        },
+        width: 25,
+        height: 25,
+        delay: 500
+    };
 
-    ctx.save();
-    ctx.drawImage(canvas, 0, 0);
-    ctx.restore();
 
-    window.requestAnimationFrame(draw);
-}
-
-window.addEventListener("load", setup);
-window.addEventListener("resize", resize);
-window.addEventListener("mousemove", mouseHandler);
-window.addEventListener("mouseout", mouseHandler);
-window.addEventListener("click", init);
+})(jQuery);
